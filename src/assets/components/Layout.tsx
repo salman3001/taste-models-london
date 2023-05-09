b87c9d24939b8e1b77ed7489346d159d0eac0d77
Salman <therodfighter@gmail.com>
2023-05-09 11:38:30 +0400

    added scroll progress

--- src/assets/components/Layout.tsx
+++ src/assets/components/Layout.tsx
@@ -4,12 +4,13 @@ import { Outlet } from "react-router-dom";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import useScrollPercentage from "react-scroll-percentage-hook";
+import { LegacyRef } from "react";
 
 const Layout = () => {
-  // const { ref, percentage } = useScrollPercentage();
+  const { ref, percentage } = useScrollPercentage({ windowScroll: true });
   return (
     <div
-      // ref={ref}
+      ref={ref as LegacyRef<HTMLDivElement>}
       className="flex relative flex-col min-h-screen justify-between text-white"
     >
       <Navbar />
@@ -17,12 +18,20 @@ const Layout = () => {
         <Outlet />
       </div>
       <Footer />
-      {/* <div className="fixed bottom-0 right-0">
-        <CircularProgressbar
-          value={percentage.vertical}
-          text={`${percentage.vertical}%`}
-        />
-      </div> */}
+      <div
+        className={`fixed flex gap-2 hover:scale-125 justify-center items-center bottom-20 right-8 h-12 w-12 transition-all cursor-pointer duration-1000  ${
+          percentage.vertical > 10 ? "opacity-100" : "opacity-0"
+        } `}
+        onClick={() => {
+          window.scrollTo({ top: 0 });
+        }}
+      >
+        <p className="text-primary uppercase">Top</p>
+        <div className="relative">
+          <CircularProgressbar value={percentage.vertical} />
+          <div className="absolute w-1 h-1 rounded-full bg-primary top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2"></div>
+        </div>
+      </div>
     </div>
   );
 };
